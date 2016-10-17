3f08cc75ac4df8994092bb9a5a71ad8d4d1b21ee
astroxslurg <kkrorkk@gmail.com>
2016-10-17 13:34:06 +0200

    added user functionality

--- client/main.js
+++ client/main.js
@@ -3,3 +3,4 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 import '/imports/ui/posts.js';
+import '/imports/startup/accounts_config.js';
---
+++ imports/startup/accounts_config.js
@@ -0,0 +1,22 @@
+import { AccountsTemplates } from 'meteor/useraccounts:core';
+
+const pwd = AccountsTemplates.removeField('password');
+AccountsTemplates.removeField('email');
+AccountsTemplates.addFields([
+  {
+    _id: "username",
+    type: "text",
+    displayName: "username",
+    required: true,
+    minLength: 3,
+  },
+  {
+    _id: 'email',
+    type: 'email',
+    required: true,
+    displayName: "email",
+    re: /.+@(.+){2,}\.(.+){2,}/,
+    errStr: 'Invalid email',
+  },
+  pwd,
+]);
